33cf4daebc28cb7235a1f39bf70580b16b5384ed
Zwerg95 <59610440+Zwerg95@users.noreply.github.com>
2021-12-05 19:09:38 +0100

    Add files via upload

---
+++ code_pirate_nettolohn/code_pirate_nettolohn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  code_pirate_nettolohn
+//
+//  Created by BHITM10 on 05.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
